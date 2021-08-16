1c4e6406e01679dc3472b969df7951f4152bfb6c
Rohan Biswas <brainhacker550@gmail.com>
2021-08-17 01:41:14 +0530

    updated font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:Semibold:size=13:autohint=true";
+static char *font = "Cascadia Code:Regular:size=13:autohint=true";
 static int borderpx = 2;
 
 /*
